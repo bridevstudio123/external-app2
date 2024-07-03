9fa3ca634be5b9b95cda338145326e4ade62311a
Murfidar <Murfidar@gmail.com>
2024-07-03 18:36:21 +0700

    chore: update

--- src/routers/home.js
+++ src/routers/home.js
@@ -6,7 +6,7 @@ home.get("/", (req, res) => {
 	// console.log("session", req.session);
 	res.render("home", {
 		title: "Home",
-		name: req.session.user ? req.session.user : "",
+		name: req.session?.user || "",
 	});
 });
 
